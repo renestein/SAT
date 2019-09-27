bf402c5171cf7d1c1e62e2b7e57c26d1a341470b
Rene Stein <rene@renestein.net>
2019-09-27 11:19:21 +0200

    Added SimpleDPLL strategy

---
+++ RSat/Core/SimpleDPLLStrategy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace RSat.Core
+{
+  public static class SimpleDPLLStrategy
+  {
+    public static Model? Solve(ImmutableList<ImmutableArray<Literal>> clausules,
+                               IDictionary<string, Variable> variablesMap)
+    {
+      ImmutableStack<SolverState> _operationStack = ImmutableStack<SolverState>.Empty;
+
+      return null;
+    }
+
+
+    private class SolverState
+    {
+
+    }
+  }
+}
